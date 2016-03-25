654e8f8d5eaced6bf4307b51d5f6863a53023970
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-03-25 19:02:19 +0100

    Event class used ad interface for events

--- myslice/db/model.py
+++ myslice/db/model.py
@@ -7,18 +7,85 @@
 ##
 
 import json
+import logging
 from enum import Enum
-from myslice.lib.util import format_date
+from rethinkdb import r, RqlDriverError
+from myslice import settings as s
+from myslice.lib.util import format_date, DecimalEncoder, DateEncoder
+
+logger = logging.getLogger("myslice.event")
 
 class EventStatus(Enum):
-    SUCCESS = 'success'
-    ERROR = 'error'
-    WARNING = 'warning'
-    PENDING = 'pending'
+    """
+        Status Class
+    """
+    ##
+    # Status for EventAction.ADD, EventAction.MOD, EventAction.DEL
+    #
+    SUCCESS = "SUCCESS"
+    ERROR = "ERROR"
+    WARNING = "WARNING"
+
+    ##
+    # Status for requests (EventAction.REQ)
+    #
+    PENDING = "PENDING"
+    REFUSED = "REFUSED"
+    APPROVED = "APPROVED"
 
 class EventAction(Enum):
-    NEW = 'new'
-    DEL = 'del'
+    ADD = "ADD"
+    DEL = "DEL"
+    MOD = "MOD"
+    REQ = "REQ"
+
+class EventObjectType(Enum):
+    AUTHORITY = "AUTHORITY"
+    PROJECT = "PROJECT"
+    SLICE = "SLICE"
+    USER = "USER"
+    RESOURCE = "RESOURCE"
+
+class EventObject(object):
+
+    def __init__(self, obj):
+        self.e = {}
+
+        self.type = obj['type']
+
+        self.id = obj['id']
+
+    def __str__(self):
+        return json.dumps(self.e, cls=DecimalEncoder, default=DateEncoder)
+
+    def dict(self):
+        return self.e
+
+    ##
+    # Object Type
+    @property
+    def type(self):
+        return self.e['type']
+
+    @type.setter
+    def type(self, value):
+        if isinstance(value, EventObjectType):
+            self.e['type'] = value.value
+        elif value in EventObjectType.__members__:
+            self.e['type'] = value
+        else:
+            raise ValueError('Event Object Type is not valid')
+
+    ##
+    # ID of the object type
+    @property
+    def id(self):
+        return self.e['id']
+
+    @id.setter
+    def id(self, value):
+        self.e['id'] = value
+
 
 class Event(object):
     """
@@ -38,6 +105,12 @@ class Event(object):
     def __init__(self, event):
         self.e = {}
 
+        ##
+        # If ID is present this is a previously
+        # created event, or we don't set it
+        if 'id' in event:
+            self.id = event['id']
+
         self.action = event['action']
 
         self.status = event['status']
@@ -47,12 +120,17 @@ class Event(object):
         else:
             self.message = ''
 
-        if not 'user' in event:
+        ##
+        # User making the request
+        #
+        if 'user' in event:
+            self.user = event['user']
+        else:
             raise ValueError('User id must be specified')
 
-        self.user = event['user']
-
-        if not 'object' in event:
+        if 'object' in event:
+            self.object = event['object']
+        else:
             raise ValueError('Object must be specified')
 
         if 'changes' in event:
@@ -69,33 +147,54 @@ class Event(object):
 
 
     def __str__(self):
-        return json.dumps(self.e)
+        return json.dumps(self.e, cls=DecimalEncoder, default=DateEncoder)
 
     def dict(self):
         return self.e
 
+    ##
+    # Id
+    @property
+    def id(self):
+        if 'id' in self.e:
+            return self.e['id']
+
+    @id.setter
+    def id(self, value):
+        self.e['id'] = value
+
+    ##
+    # Action
     @property
     def action(self):
         return self.e['action']
 
     @action.setter
     def action(self, value):
-        if not isinstance(value, EventAction):
-            raise ValueError('Action not valid')
+        if isinstance(value, EventAction):
+            self.e['action'] = value.value
+        elif value in EventAction.__members__:
+            self.e['action'] = value
+        else:
+            raise ValueError('Event Action not valid')
 
-        self.e['action'] = value.value
-
+    ##
+    # Status
     @property
     def status(self):
         return self.e['status']
 
-    @status.setter
+    @action.setter
     def status(self, value):
-        if not isinstance(value, EventStatus):
-            raise ValueError('Status not valid')
+        if isinstance(value, EventStatus):
+            self.e['status'] = value.value
+        elif value in EventStatus.__members__:
+            self.e['status'] = value
+        else:
+            raise ValueError('Event Status not valid')
 
-        self.e['status'] = value.value
-
+    ##
+    # Message
     @property
     def message(self):
         return self.e['message']
@@ -104,6 +203,8 @@ class Event(object):
     def message(self, value=''):
         self.e['message'] = value
 
+    ##
+    # User
     @property
     def user(self):
         return self.e['user']
@@ -112,17 +213,18 @@ class Event(object):
     def user(self, value):
         self.e['user'] = value
 
+    ##
+    # Object
     @property
     def object(self):
         return self.e['object']
 
     @object.setter
     def object(self, value):
-        if not all(k in value for k in ("type")):
-            raise ValueError('Object must have at least type')
+        self.e['object'] = EventObject(value).dict()
 
-        self.e['object'] = value
-
+    ##
+    # Changes
     @property
     def changes(self):
         return self.e['changes']
@@ -140,3 +242,36 @@ class Event(object):
         else:
             self.e['updated'] = format_date()
 
+    ##
+    # Dispatches the event. This uses rethinkdb as backend
+    def dispatch(self):
+        # update date of the event
+        self.updated()
+
+        # connect to db
+        logger.info("Connecting to db {} on {}:{}".format(s.db.name, s.db.host, s.db.port))
+        try:
+            c = r.connect(host=s.db.host, port=s.db.port, db=s.db.name)
+        except RqlDriverError:
+            logger.error("Can't connect to RethinkDB")
+            raise SystemExit("Can't connect to RethinkDB")
+
+        if self.id:
+            ##
+            # updating existing event
+            ret = r.db(s.db.name).table('events').get(self.id).update(self.dict()).run(c)
+        else:
+            ##
+            # dispatching new event
+            ret = r.db(s.db.name).table('events').insert(self.dict()).run(c)
+
+        import pprint
+        pprint.pprint(ret)
+        if ret['errors'] > 0:
+            self.message = ret['first_error']
+            self.status = EventStatus.ERROR
+            return False
+        elif ret['inserted'] > 0:
+            self.id = ret['generated_keys'][0]
+
+        return True
