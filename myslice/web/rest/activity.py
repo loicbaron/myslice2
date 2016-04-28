fbff160ab7d702bdd6efaf05485c27534dbb31f2
Quan Zhou <quan.zhou@lip6.fr>
2016-04-28 16:00:35 +0200

    rest api have response now

--- myslice/web/rest/activity.py
+++ myslice/web/rest/activity.py
@@ -5,8 +5,8 @@ from tornado import gen, escape
 
 from myslice.web.rest import Api
 from myslice.lib.util import myJSONEncoder
-from myslice.db import dispatch
-from myslice.db.activity import Event
+from myslice.db import dispatch, changes
+from myslice.db.activity import Event, EventStatus
 
 class ActivityHandler(Api):
 
@@ -41,19 +41,32 @@ class ActivityHandler(Api):
         try:
             data = escape.json_decode(self.request.body)['event']
         except json.decoder.JSONDecodeError as e:
-            self.set_status(500)
             self.finish(json.dumps({"return": {"status": "error", "messages": "malformed request"}}))
             return
 
         try:
             event = Event(data)
         except Exception as e:
-            self.set_status(500)
             self.finish(json.dumps({"return": {"status":"error","messages":e.message}}))
-            #import traceback
-            #traceback.print_exc()
+            import traceback
+            traceback.print_exc()
         else:
             result = yield dispatch(self.dbconnection, event)
             #data = self.get_argument('event','no data')
-            print(event)
+            event_id = result['generated_keys'][0]
+            feed = yield changes(dbconnection = self.dbconnection, table='activity')
+            while (yield feed.fetch_next()):
+                item = yield feed.next()
+                ev = Event(item['new_val'])
+                if ev.id == event_id:
+                    print(ev.status)
+                    if ev.status == EventStatus.ERROR:
+                        self.set_status(400)
+                        self.finish(json.dumps({"return": {"status":"success","messages":ev.log}}, cls=myJSONEncoder))
+                    if ev.status == EventStatus.SUCCESS:
+                        print('ok')
+                        self.set_status(200)
+                        self.finish(json.dumps({"return": {"status":"success","messages":ev}}, cls=myJSONEncoder))
+
+
 
