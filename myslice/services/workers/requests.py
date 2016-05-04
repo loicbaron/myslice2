c49484674691c69f61230fbe43c2dfd5b9e3a6f1
Quan Zhou <quan.zhou@lip6.fr>
2016-05-04 11:04:12 +0200

    add create user

--- myslice/services/workers/requests.py
+++ myslice/services/workers/requests.py
@@ -7,15 +7,24 @@
 ##
 
 import logging
+
+from myslice import db
+from myslice import settings as s
 from myslice.db import connect, users
 from myslice.db.activity import Event 
+from myslice.db.authority import Authority
+from myslice.db.user import User
 from myslice.db.message import PortalMessage
 from myslice.db.message import Mailer
-from myslice import settings as s
-
 
 logger = logging.getLogger('myslice.service.requests')
 
+def get_authority_id(usr_id):
+    if usr_id.startswith('urn:publicid:IDN+'):
+        authority = usr_id.split('+')[1]
+        return "urn:publicid:IDN+{}+authority+sa".format(authority)
+
+    raise TypeError("Object id is not a valid id")
 
 def run(qRequests):
     """
@@ -33,4 +42,30 @@ def run(qRequests):
         except Exception as e:
             logger.error("Problem with event: {}".format(e))
         else:
-            user = db.user
+            try:
+                pis_email = []
+                if event.user is None:
+                    auth_id = get_authority_id(event.object.id)
+                    authority = Authority(db.get(dbconnection, table='authorities', id=auth_id))
+                    for pi_user in authority.pi_users:
+                        user = User(db.get(dbconnection, table='users', id=pi_user))
+                        pis_email.append(user.email)
+
+                    m = PortalMessage(author=('OneLab Support', 'zhouquantest16@gmail.com'),
+                                    to = pis_email,
+                                    entity = event.object.type.lower(),
+                                    action = 'request',
+                                    loader_path = s.email.dirpath,
+                                    theme = s.email.theme,
+                                    )
+                    m.generate_message(
+                                url = "http://oneLab.eu",
+                                first_name = event.data['first_name'],
+                                last_name = event.data['last_name'],
+                                email = event.data['email']
+                                )
+                    mailer = Mailer().send(m)
+
+            except Exception as e:
+                event.setError()
+                logger.error("Unable to fetch the user from db {}".format(e))
