8836faf5c00493382c9d170faa053e0fb3a11900
Quan Zhou <quan.zhou@lip6.fr>
2016-06-09 18:00:55 +0200

    home profile

--- myslice/web/controllers/home.py
+++ myslice/web/controllers/home.py
@@ -10,3 +10,8 @@ class Project(BaseController):
     def get(self):
         self.set_current_user("test")
         self.render(self.application.templates + "/project.html")
+
+class User(BaseController):
+    def get(self):
+        self.set_current_user("test")
+        self.render(self.application.templates + "/profile.html")
