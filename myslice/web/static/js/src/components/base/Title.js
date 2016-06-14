01eb64ee2b8184a90ef67aef7d1710352f2e4bb3
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-06-14 18:50:43 +0200

    Supports sub-title

--- myslice/web/static/js/src/components/base/Title.js
+++ myslice/web/static/js/src/components/base/Title.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-export default class Title extends React.Component {
+class Title extends React.Component {
   render() {
         return (
             <div className="p-title">
                 <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
   }
 }
+
+export default Title;
