dffa9235e025cc7f797ffbb9dd7819823983c329
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-06-14 15:05:27 +0200

    update Projects (view) component

--- myslice/web/static/js/src/projects.js
+++ myslice/web/static/js/src/projects.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ProjectsList from './components/ProjectsList';
 import ProjectsView from './components/ProjectsView';
-
-ReactDOM.render(
-        <ProjectsList />,
-        document.getElementById('projectsList')
-);
 
 ReactDOM.render(
         <ProjectsView />,
