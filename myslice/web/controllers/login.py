71f5be1316a82c16feacccfc4abf01101e7b4e10
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-06-15 10:51:14 +0200

    rest login controller

--- myslice/web/controllers/login.py
+++ myslice/web/controllers/login.py
@@ -1,20 +1,13 @@
-from tornado import web, escape
+from email.utils import parseaddr
+import crypt
+from hmac import compare_digest as compare_hash
+from tornado.web import MissingArgumentError
+from tornado import gen
+from rethinkdb import r
 
-class BaseHandler(web.RequestHandler):
+from myslice.web.controllers import BaseController
 
-    def get_current_user(self):
-        return self.get_secure_cookie("user")
-
-class MainHandler(BaseHandler):
-
-    def get(self):
-        if not self.current_user:
-            self.redirect("/login")
-            return
-        name = escape.xhtml_escape(self.current_user)
-        self.write("Hello, " + name)
-
-class Index(BaseHandler):
+class Index(BaseController):
 
     def get(self):
         """
@@ -22,14 +15,50 @@ class Index(BaseHandler):
 
         :return:
         """
-        self.render(self.application.templates + "/login.html")
+        self.render(self.application.templates + "/login.html", message='')
 
-
+    @gen.coroutine
     def post(self):
         """
             Authentication
         """
-        self.set_secure_cookie("user", self.get_argument("name"))
+
+        try:
+            post_email = self.get_argument("email")
+        except MissingArgumentError as e:
+            self.render(self.application.templates + "/login.html", message="email missing")
+            return
+
+        try:
+            post_password = self.get_argument("password")
+        except MissingArgumentError as e:
+            self.render(self.application.templates + "/login.html", message="password missing")
+            return
+
+        _, email = parseaddr(post_email)
+        if not email:
+            self.render(self.application.templates + "/login.html", message="wrong email")
+            return
+
+        if not post_password:
+            self.render(self.application.templates + "/login.html", message="empty password")
+            return
+        else:
+            password = post_password
+
+        feed = yield r.table('users').filter({"email" : email}).run(self.application.dbconnection)
+        yield feed.fetch_next()
+        user = yield feed.next()
+
+        if not user:
+            self.render(self.application.templates + "/login.html", message="user does not exist")
+            return
+
+        if not compare_hash(crypt.crypt(password, user['password']), user['password']):
+            self.render(self.application.templates + "/login.html", message="password does not match")
+            return
+
+        self.set_current_user(user['id'])
         self.redirect("/")
 
 
