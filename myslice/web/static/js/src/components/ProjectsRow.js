0103402cb9baaf37ff34def5a8ac9a239d112f9f
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-06-17 21:10:19 +0200

    project element style and icon

--- myslice/web/static/js/src/components/ProjectsRow.js
+++ myslice/web/static/js/src/components/ProjectsRow.js
@@ -5,6 +5,7 @@ import Element from './base/Element';
 import ElementTitle from './base/ElementTitle';
 import ElementId from './base/ElementId';
 import ElementStatus from './base/ElementStatus';
+import ElementIcon from './base/ElementIcon';
 import DateTime from './base/DateTime';
 
 class ProjectLabel extends React.Component {
@@ -23,9 +24,7 @@ class ProjectLabel extends React.Component {
 
             <div className="row">
                 <div className="col-md-12">
-                    <div className="elementIcon">
-                <img src="/static/icons/projects-w-24.png" alt="" />
-            </div>
+
                     <div className="elementLabel">{ this.label() }</div>
                 </div>
             </div>
@@ -60,6 +59,7 @@ class ProjectsRow extends React.Component {
 
         return (
              <Element element={this.props.project}>
+                 <ElementIcon icon="project" />
                  <ElementTitle label={label} detail={this.props.project.shortname} />
                  <ElementId id={this.props.project.id} />
                  <div className="elementDetail">
@@ -69,12 +69,22 @@ class ProjectsRow extends React.Component {
                      <br />
                      <span className="elementLabel">Managed by</span> {authority}
                  </div>
-                 <div className="elementDate">
-                     <span className="elementLabel">Created</span> <DateTime timestamp={this.props.project.created} />
-                     <br />
-                     <span className="elementLabel">Enabled</span> <DateTime timestamp={this.props.project.enabled} />
-                     <br />
-                     <span className="elementLabel">Updated</span> <DateTime timestamp={this.props.project.updated} />
+                 <div className="row elementDate">
+                     <div className="col-sm-3">
+                         <span className="elementLabel">Created</span>
+                         <br />
+                         <DateTime timestamp={this.props.project.created} />
+                     </div>
+                     <div className="col-sm-3">
+                         <span className="elementLabel">Enabled</span>
+                         <br />
+                         <DateTime timestamp={this.props.project.enabled} />
+                     </div>
+                     <div className="col-sm-3">
+                         <span className="elementLabel">Updated</span>
+                         <br />
+                         <DateTime timestamp={this.props.project.updated} />
+                     </div>
                  </div>
                  <ElementStatus status={this.props.project.status} />
              </Element>
---
+++ myslice/web/static/js/src/components/base/ElementIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ElementIcon extends React.Component {
+  render() {
+        return (
+            <div className="elementIcon iconProject">
+                <i className="fa fa-flask fa-2x"></i>
+            </div>
+        );
+  }
+}
+
+export default ElementIcon;
