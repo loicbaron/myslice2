d18cffc152025c8520269c20b0e1e3c034977a20
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-06-22 19:07:27 +0200

    improved auth

--- myslice/web/controllers/__init__.py
+++ myslice/web/controllers/__init__.py
@@ -1,28 +1,22 @@
 import logging
+import json
 from tornado import web, escape
-from rethinkdb import r
 
-from myslice.db.user import User
 
 logger = logging.getLogger(__name__)
 
 class BaseController(web.RequestHandler):
 
-    def set_current_user(self, id):
-        if id:
-            self.set_secure_cookie("user", id)
-        else:
-            self.clear_cookie("user")
-
     def get_current_user(self):
-        return self.get_secure_cookie("user")
-        #logger.debug("COOKIE USER ID: {}".format(cookie))
 
-    def get_current_user_obj(self):
-        ret = yield r.table('user').get(self.get_current_user_id()).run(self.dbconnection)
-        if not ret:
-            self.redirect("/login")
+        cookie = self.get_secure_cookie("user")
+        if not cookie:
+            return False
 
-        user = User(ret)
+        user = json.loads(str(cookie, "utf-8"))
 
-        yield user
+        if not isinstance(user, dict):
+            self.clear_cookie('user')
+            self.redirect('/login')
+
+        return user
