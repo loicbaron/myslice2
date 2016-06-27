1baf6e7f3739abcc4444fb27873c93d6479edede
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-06-27 14:30:50 +0200

    updated webpack config

--- myslice/web/static/js/src/webpack.config.js
+++ myslice/web/static/js/src/webpack.config.js
@@ -3,34 +3,35 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  context: __dirname,
-  entry: {
-          settings :"./settings.js",
-          password :"./password.js",
-          projects: "./projects.js",
-          users: "./users.js",
-          registration: "./registration.js",
-          login: "./login.js",
-          activity: "./activity.js"
-        },
-  output: {
-    filename: "[name].js",
-    path: "../build/",
-  },
-  module: {
+    context: __dirname,
+    entry: {
+        settings :"./settings.js",
+        password :"./password.js",
+        projects: "./projects.js",
+        users: "./users.js",
+        registration: "./registration.js",
+        login: "./login.js",
+        navbar: "./navbar.js",
+        activity: "./activity.js"
+    },
+    output: {
+        filename: "[name].js",
+        path: "../build/",
+    },
+    module: {
     loaders: [
         {
-          test: /\.jsx?$/,
-          include: [
-            __dirname,
-          ],
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-           query: {
-            presets: ['es2015', 'react'],
-            compact: false
-           }
+            test: /\.jsx?$/,
+            include: [
+                __dirname,
+            ],
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react'],
+                compact: false
+            }
         }, { test: /\.css$/, loader: 'style-loader!css-loader' },
     ]
-   },
+    },
 };
