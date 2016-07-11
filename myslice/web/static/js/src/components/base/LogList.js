bd1265c6deeba2a0ad39b567be52a728b10f8abe
Quan Zhou <quan.zhou@lip6.fr>
2016-07-11 11:34:47 +0200

    fix bug in loglist

--- myslice/web/static/js/src/components/base/LogList.js
+++ myslice/web/static/js/src/components/base/LogList.js
@@ -5,11 +5,16 @@ import LogRow from './LogRow';
 class LogList extends React.Component {
 
     render() {
+        
+        this.props.log.sort(function(x, y) {
+                return new Date(y.timestamp).getTime() - new Date(x.timestamp).getTime();
+        })
+
         return (
             <ul className="logList">
             {
                  this.props.log.map(function(log) {
-                     return <LogRow key={log.timestamp} log={log} />;
+                     return <LogRow  key={log.timestamp + log.type} log={log} />;
                  })
              }
             </ul>
