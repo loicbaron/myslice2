92884872ac86f54112b89bf85a9bb17be81d2ca9
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2016-07-26 19:44:22 +0200

    get current slice from the url

--- myslice/web/static/js/src/slice.js
+++ myslice/web/static/js/src/slice.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SliceView from './components/slice/View';
 
+const getCurrentSlice = () => {
+    var u = window.location.href.toString().split(window.location.host)[1].split('/');
+    var hrn = u.pop();
+    var ctl = u.pop();
+
+    if (ctl === 'slices') {
+        return hrn;
+    }
+};
+
 ReactDOM.render(
-        <SliceView />,
+        <SliceView slice={getCurrentSlice()} />,
         document.getElementById('sliceView')
 );
