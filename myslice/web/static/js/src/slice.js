35bb90ac3aba5ea4f20798cab2ed829a249852e9
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2016-07-26 13:50:28 +0200

    removed old icons

---
+++ myslice/web/static/js/src/slice.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SliceView from './components/slice/View';
+
+ReactDOM.render(
+        <SliceView />,
+        document.getElementById('sliceView')
+);
