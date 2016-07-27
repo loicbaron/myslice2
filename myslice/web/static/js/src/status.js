e5ed556b9d577a6902477efd13ced787a3affc3b
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2016-07-27 21:37:37 +0200

    status templates

---
+++ myslice/web/static/js/src/status.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StatusView from './components/status/View';
+
+ReactDOM.render(
+        <StatusView />,
+        document.getElementById('statusView')
+);
