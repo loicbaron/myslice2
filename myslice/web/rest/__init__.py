e2721fbcea07c4dc36d4f966947e034905064f02
Quan Zhou <quan.zhou@lip6.fr>
2016-08-25 20:27:36 +0200

    fix error

--- myslice/web/rest/__init__.py
+++ myslice/web/rest/__init__.py
@@ -45,16 +45,18 @@ class Api(cors.CorsMixin, web.RequestHandler):
 
     def isAdmin(self):
         auth_pattern = re.compile(r"(urn:publicid:IDN\+)(?P<hrn>[\:]*[a-zA-Z]*)(\+authority\+sa)")
-        
-        # XXX not sure if it is a clean way to decide a admin
-        pi_auth = self.get_current_user()['pi_authorities']
-        for auth in pi_auth:
-            m = auth_pattern.match(auth)
-            hrn_length = len(m.group('hrn').split(':'))
-            if hrn_length == 1:
-                return True
-
-        return False
+        try:
+            # XXX not sure if it is a clean way to decide a admin
+            pi_auth = self.get_current_user()['pi_authorities']
+            for auth in pi_auth:
+                m = auth_pattern.match(auth)
+                hrn_length = len(m.group('hrn').split(':'))
+                if hrn_length == 1:
+                    return True
+        except Exception as e:
+            self.serverError("enable to identify user permission")
+        finally:
+            return False
 
     def isUrn(self, urn):
         return re.match(self.application.urn_regex, urn)
