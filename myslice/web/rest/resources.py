997f5e061e1ed4802a26c528a9964d8ca055385c
Amira <amira.bradai@lip6.fr>
2016-09-19 10:47:57 +0200

    get resources       resources/id ( +using isUrn(id) )       resources/id/leases( +using isUrn(id) )

--- myslice/web/rest/resources.py
+++ myslice/web/rest/resources.py
@@ -1,44 +1,65 @@
 import json
 
 import rethinkdb as r
-from tornado import gen
 
-from myslice.web.rest import Api
 from myslice.lib.util import myJSONEncoder
+from myslice.web.rest import Api
 
+from myslice.db.activity import Event, EventAction, ObjectType
+from myslice.db import dispatch
+
+from tornado import gen, escape
 
 class ResourcesHandler(Api):
 
     @gen.coroutine
     def get(self, id=None, o=None):
         """
-        GET /resources/[<id>]
+            - GET /resources
+                (public) resources list
 
-        Resource list or resource with <id>
+            - GET /resources/<id>
+                (public) Resources with <id>
 
-        GET /resources/<id>/leases
+            - GET /resources/<id>/leases
+                Leases list of the resource with the <id>
+            :return:
+            """
 
-        leases list with resourse id
-        :return:
-        """
-        resources = []
+        response = []
+        current_user = self.get_current_user()
 
-        # TODO: id must be a valid URN
-        if id and not o:
-            cursor = yield r.table('resources').run(self.dbconnection)
+        # GET /resources
+        if not id and not o:
+            cursor = yield r.table('resources') \
+                .run(self.dbconnection)
             while (yield cursor.fetch_next()):
-                result = yield cursor.next()
-                resources.append(result)
+                item = yield cursor.next()
+                response.append(item)
 
-        #  GET /resources/<id>/leases
-        elif o == 'leases':
-            cursor = yield r.table(o).filter(lambda lease: lease["resources"] == id).run(self.dbconnection)
 
+        # GET /resources/<id>
+        elif not o and id and self.isUrn(id):
+
+            cursor = yield r.table('resources') \
+                .filter({'id': id}) \
+                .run(self.dbconnection)
+            while (yield cursor.fetch_next()):
+                item = yield cursor.next()
+                response.append(item)
+        # GET /resources/<id>/leases
+        elif id and self.isUrn(id) and o == 'leases':
+            cursor = yield r.table(o) \
+                .filter(lambda lease: lease["resources"].contains(id)) \
+                .run(self.dbconnection)
+                #
 
             while (yield cursor.fetch_next()):
                 item = yield cursor.next()
-                resources.append(item)
-        else :
-             self.userError("invalid request")
-             return
-        self.write(json.dumps({"result": resources}, cls=myJSONEncoder))
+                response.append(item)
+        else:
+            self.userError("invalid request")
+
+            return
+
+        self.finish(json.dumps({"result": response}, cls=myJSONEncoder))
