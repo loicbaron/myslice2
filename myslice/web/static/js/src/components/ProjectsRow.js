4931495e8e0075ed0faeb002d1874cd89937b133
Amira <amira.bradai@lip6.fr>
2016-10-21 11:33:57 +0200

    fixing warnings after update project

--- myslice/web/static/js/src/components/ProjectsRow.js
+++ myslice/web/static/js/src/components/ProjectsRow.js
@@ -4,7 +4,6 @@ import Element from './base/Element';
 import ElementTitle from './base/ElementTitle';
 import ElementId from './base/ElementId';
 import ElementStatus from './base/ElementStatus';
-import ElementOption from './base/ElementOption';
 import ElementIcon from './base/ElementIcon';
 import DateTime from './base/DateTime';
 import SlicesRow from'./SlicesRow';
@@ -38,7 +37,7 @@ class ProjectsRow extends React.Component {
             {
                 this.props.project.slices.map(function (slice) {
                     var slice_link="/slices/"+slice.hrn;
-                    return <span key={slice.id} className="elementLabel"><a href={slice_link}>{slice.shortname}</a></span>;
+                    return <span key={slice.id } className="elementLabel"><a href={slice_link}>{slice.shortname}</a></span>;
                 }.bind(this))
             }
             </div>;
@@ -50,11 +49,11 @@ class ProjectsRow extends React.Component {
         }
         var enabled;
         if(this.props.project.enabled){
-            enabled = <div className="col-sm-3"><DateTime label="Enabled" timestamp={this.props.project.enabled}/></div>
+            created = <div className="col-sm-3"><DateTime label="Enabled" timestamp={this.props.project.enabled}/></div>
         }
         var updated;
         if(this.props.project.updated){
-            updated = <div className="col-sm-3"><DateTime label="Updated" timestamp={this.props.project.updated}/></div>
+            created = <div className="col-sm-3"><DateTime label="Updated" timestamp={this.props.project.updated}/></div>
         }
         var authorityElement;
         if(authority){
@@ -66,7 +65,8 @@ class ProjectsRow extends React.Component {
         }
         if (this.props.detailed) {
             return (
-                <Element element={this.props.project} type="project" handleClick={this.props.handleClick} minHeight={minHeight}>
+                <Element element={this.props.project} type="project" setCurrent={this.props.setCurrent}
+                         current={this.props.current} minHeight={minHeight}>
                     <ElementStatus status={this.props.project.status}/>
                     <ElementIcon icon="project"/>
                     <ElementTitle label={label} detail={this.props.project.shortname}/>
@@ -84,7 +84,8 @@ class ProjectsRow extends React.Component {
         else
             {
             return (
-                <Element element={this.props.project} type="project" handleClick={this.props.setCurrent} minHeight={minHeight}>
+                <Element element={this.props.project} type="project" setCurrent={this.props.setCurrent}
+                         current={this.props.current} minHeight={minHeight}>
                     <ElementIcon icon="project"/>
                     <a href="/projects"><ElementTitle label={label} detail={this.props.project.shortname} /></a>
 
@@ -101,12 +102,14 @@ class ProjectsRow extends React.Component {
 
 ProjectsRow.propTypes = {
     project: React.PropTypes.object.isRequired,
-    handleClick: React.PropTypes.func,
+    current: React.PropTypes.object,
+    setCurrent: React.PropTypes.func,
     removeProject: React.PropTypes.bool,
     detailed: React.PropTypes.bool,
 };
 
 ProjectsRow.defaultProps = {
+    current: false,
     removeProject: true,
     setCurrent: null,
     detailed: true,
