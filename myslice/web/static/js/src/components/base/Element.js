b2f1a06b55cafacc27eff2d657fc921f49c47e13
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2016-11-06 22:45:13 +0100

    Refactored Element component

--- myslice/web/static/js/src/components/base/Element.js
+++ myslice/web/static/js/src/components/base/Element.js
@@ -1,62 +1,129 @@
 import React from 'react';
+import Icon from './Icon';
 
-const Element = (props) => {
+const ElementIcon = ({icon}) => {
 
-    var className = 'elementBox';
-    var style;
-    var callback = null;
 
-    if (props.minHeight) {
-        style = props.minHeight;
-    }
+};
 
-    if (props.type) {
-        className += ' ' + props.type;
-    }
+ElementIcon.defaultProps = {
+    icon: null
+};
 
-    if (props.handleClick) {
-        callback = () => props.handleClick(props.element);
-        className += ' pointer';
-    }
+const ElementStatus = ({status}) => {
 
-    if ((typeof(props.element.isCurrent) === 'boolean' && (props.element.isCurrent))) {
-        className += ' selected';
         return (
-            <li className={className} onClick={callback} style={style}>
-                {props.children}
-                <i className="fa fa-arrow-right fa-lg arrow-right"></i>
+                <div className="elementStatus">
+                    <Icon name={status} />&nbsp;{status}
+                </div>
+        );
+
+};
+
+const ElementControl = ({status, options}) => {
+    return <div className="elementControl">
+        <ElementStatus status={status} />
+        {
+            options.map((option) => {
+                return option;
+            })
+        }
+    </div>;
+};
+
+ElementControl.defaultProps = {
+    status: null,
+    options: []
+};
+
+class Element extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.isSelected !== nextProps.isSelected;
+    }
+
+    renderIcon() {
+        var icon = this.props.icon;
+
+        if (!icon) {
+            icon = this.props.type;
+        }
+
+        return (
+            <div className={"elementIcon " + icon}>
+                <Icon name={icon} size="2x" />
+            </div>
+        );
+    }
+    renderIconSelected() {
+        if (this.props.isSelected) {
+            switch (this.props.iconSelected) {
+                case 'arrow':
+                    return <i className="fa fa-arrow-right fa-lg arrow-right"></i>;
+                    break;
+                case 'check':
+                    return <i className="fa fa-check-square-o fa-lg check-right"></i>;
+                    break;
+            }
+        }
+    }
+
+    render() {
+        var className = 'elementBox';
+        var style;
+        var callback = null;
+        var options = this.props.options;
+        var status = this.props.status;
+
+
+        if (this.props.type) {
+            className += ' ' + this.props.type;
+        }
+
+        if (this.props.handleSelect) {
+            callback = () => this.props.handleSelect(this.props.element);
+            className += ' pointer';
+        }
+
+        if (this.props.isSelected) {
+            className += ' selected';
+        }
+
+        return (
+            <li className={className} onClick={callback} style={this.props.minHeight}>
+                {this.renderIcon()}
+                <ElementControl status={status} options={options}/>
+                {this.props.children}
+                {this.renderIconSelected()}
             </li>
         );
     }
-
-    if ((typeof(props.element.isSelected) === 'boolean' && (props.element.isSelected))) {
-        className += ' selected';
-        return (
-            <li className={className} onClick={callback} style={style}>
-                {props.children}
-                <i className="fa fa-check-square-o fa-lg check-right"></i>
-            </li>
-        );
-    }
-
-    return (
-        <li className={className} onClick={callback} style={style}>
-            {props.children}
-        </li>
-    );
 };
 
 Element.propTypes = {
     element: React.PropTypes.object.isRequired,
     type: React.PropTypes.string,
+    icon: React.PropTypes.string,
+    iconSelected: React.PropTypes.string,
     isSelected: React.PropTypes.bool,
-    isChecked: React.PropTypes.bool,
     handleClick: React.PropTypes.func
 };
 
 Element.defaultProps = {
     type: null,
-    current: null
+    icon: null,
+    iconSelected: 'check',
+    isSelected: false,
+    status: null,
+    options: [],
 };
 
 export default Element;
