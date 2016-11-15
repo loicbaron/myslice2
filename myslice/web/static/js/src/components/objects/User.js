c55d0aec78578f2433c96efd816d8191f450b2d7
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2016-11-15 19:24:59 +0100

    User Element and Lists improvements, support for multiple selection

--- myslice/web/static/js/src/components/objects/User.js
+++ myslice/web/static/js/src/components/objects/User.js
@@ -7,70 +7,100 @@ import ElementTitle from '../base/ElementTitle';
 import ElementId from '../base/ElementId';
 import DateTime from '../base/DateTime';
 
-import AddUserToProject from '../AddUserToProject';
-import RemoveUserFromProject from '../RemoveUserFromProject';
+// import AddUserToProject from '../AddUserToProject';
+// import RemoveUserFromProject from '../RemoveUserFromProject';
 
-class UserElement extends React.Component {
-    constructor(props) {
-        super(props);
+const UserElement = ({user, isSelected, handleSelect}) => {
+
+    var authority = user.authority.name || user.authority.shortname;
+
+    var num_slices = 0;
+    if (user.hasOwnProperty('slices')) {
+        num_slices = user.slices.length
     }
-    render() {
-        var authority = this.props.user.authority.name || this.props.user.authority.shortname;
 
-        var num_slices = 0;
-        if (this.props.user.hasOwnProperty('slices')) {
-            num_slices = this.props.user.slices.length
-        }
-
-        var num_projects = 0;
-        if (this.props.user.hasOwnProperty('projects')) {
-            num_projects = this.props.user.projects.length
-        }
-        var button = '';
-        if(this.props.addUser){
-            button = <AddUserToProject user={this.props.user} />
-        }
-        if(this.props.removeUser){
-            button = <RemoveUserFromProject user={this.props.user} />
-        }
-
-        var fullname = [this.props.user.first_name, this.props.user.last_name].join(' ');
-        if (!fullname) {
-            fullname = this.props.user.shortname;
-        }
-        return (
-             <Element element={this.props.user}
-                      type="user"
-                      setCurrent={this.props.setCurrent}
-                      current={this.props.current}
-                      status={this.props.user.status}
-             >
-                 <Avatar className="elementIcon" email={this.props.user.email} name={fullname} round={true} size={40} color="#CFE2F3" />
-                 <ElementTitle label={fullname} detail={this.props.user.email} />
-                 <ElementId id={this.props.user.id} />
-                 {button}
-                 <div className="elementDetail">
-                     <span className="elementLabel">Projects</span> {num_projects}
-                     &nbsp;&nbsp;&nbsp;&nbsp;
-                     <span className="elementLabel">Slices</span> {num_slices}
-                     <br />
-                     <span className="elementLabel">Part of</span> {authority}
-                 </div>
-             </Element>
-         );
+    var num_projects = 0;
+    if (user.hasOwnProperty('projects')) {
+        num_projects = user.projects.length
     }
- }
+
+        // var button = '';
+        // if(this.props.addUser){
+        //     button = <AddUserToProject user={this.props.user} />
+        // }
+        // if(this.props.removeUser){
+        //     button = <RemoveUserFromProject user={this.props.user} />
+        // }
+
+    var fullname = [ user.first_name, user.last_name ].join(' ');
+    if (!fullname) {
+        fullname = user.shortname;
+    }
+
+    var status;
+    if (user.available == 'true') {
+        status = 'online';
+    } else {
+        status = 'offline';
+    }
+
+    return (
+        <Element type="user"
+                 element={user}
+                 isSelected={isSelected}
+                 handleSelect={handleSelect}
+                 status={status}
+                 icon="user"
+                 iconSelected="check"
+        >
+            {/*<Avatar className="elementIcon" email={this.props.user.email} name={fullname} round={true} size={40} color="#CFE2F3" />*/}
+            <ElementTitle label={fullname} detail={user.email} />
+            <ElementId id={user.id} />
+            <div className="elementDetail">
+                 <span className="elementLabel">Projects</span> {num_projects}
+                 &nbsp;&nbsp;&nbsp;&nbsp;
+                 <span className="elementLabel">Slices</span> {num_slices}
+                 <br />
+                 <span className="elementLabel">Organization</span> {authority}
+            </div>
+         </Element>
+     );
+
+ };
 
 UserElement.propTypes = {
-    user: React.PropTypes.object.isRequired,
-    addUser: React.PropTypes.bool,
-    removeUser: React.PropTypes.bool,
+    user: React.PropTypes.object.isRequired
 };
 
 UserElement.defaultProps = {
-    addUser: false,
-    removeUser: false,
 };
+
+const UserList = ({users, selected, handleSelect}) =>
+    <List>
+    {
+        users.map(function(user) {
+            var isSelected = false;
+            if (selected) {
+                 isSelected = selected.some(function (el) {
+                    return el.id === user.id;
+                });
+            }
+
+            return <UserElement key={user.id} user={user} isSelected={isSelected} handleSelect={handleSelect} />;
+        }.bind(this))
+    }
+    </List>;
+
+UserList.propTypes = {
+    users: React.PropTypes.array.isRequired,
+};
+
+UserList.defaultProps = {
+};
+
+
+
+
 
 const UserElementSimple = ({user}) => {
 
@@ -82,7 +112,7 @@ const UserElementSimple = ({user}) => {
     }
 
     return <Element element={user} type="user">
-                <Avatar className="elementIcon" email={user.email} name={fullname} round={true} size={35} color="#CFE2F3" />
+                {/*<Avatar className="elementIcon" email={user.email} name={fullname} round={true} size={35} color="#CFE2F3" />*/}
                 <ElementTitle label={fullname} detail={user.email} />
             </Element>;
 };
@@ -92,43 +122,6 @@ UserElementSimple.propTypes = {
 };
 
 UserElementSimple.defaultProps = {
-};
-
-class UserList extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <List>
-                {
-                    this.props.users.map(function(user) {
-                        return <UserElement key={user.id}
-                                            user={user}
-                                            setCurrent={this.props.setCurrent}
-                                            current={this.props.current}
-                                            addUser={this.props.addUser}
-                                            removeUser={this.props.removeUser} />;
-                    }.bind(this))
-                }
-                </List>
-            </div>
-        );
-    }
-}
-
-UserList.propTypes = {
-    users: React.PropTypes.array.isRequired,
-    current: React.PropTypes.object,
-    addUser: React.PropTypes.bool,
-    removeUser: React.PropTypes.bool,
-    setCurrent: React.PropTypes.func
-};
-
-UserList.defaultProps = {
-    current: null,
-    setCurrent: null,
-    addUser: false,
-    removeUser: false,
 };
 
 const UserListSimple = ({users}) =>
