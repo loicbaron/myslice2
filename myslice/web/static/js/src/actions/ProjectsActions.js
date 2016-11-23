961ab84d862ba49f015c975358f7ff4f6cf37e89
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2016-11-23 12:38:04 +0100

    refactoring: simplified action calls

--- myslice/web/static/js/src/actions/ProjectsActions.js
+++ myslice/web/static/js/src/actions/ProjectsActions.js
@@ -2,85 +2,41 @@ import alt from '../alt';
 
 class ProjectsActions {
 
-    fetchProjects(filter = {}) {
-        return filter;
+    constructor() {
+        this.generateActions(
+            'addUsers',
+
+            'fetchProjects',
+            'updateProjectElement',
+            'setCurrentProject',
+            'updateProjects',
+            'errorProjects',
+            'updateUsers',
+            'addUser',
+            'updateAddUser',
+            'errorAddUser',
+            'removeUser',
+            'updateRemoveUser',
+            'errorRemoveUser',
+            'deleteSlice',
+            'updateDeleteSlice',
+            'errorDeleteSlice',
+            'errorUsers',
+            'updateSlices',
+            'errorSlices',
+            'showDialog',
+
+            'deleteProject',
+            'updateDeleteProject',
+            'errorDeleteProject',
+
+
+            'saveProject',
+            'successSave',
+            'errorSave'
+        );
     }
 
-    updateProjectElement(project) {
-        return project;
-    }
-
-    setCurrentProject(project) {
-        return project;
-    }
-
-    updateProjects(project) {
-        return project;
-    }
-
-    errorProjects(errorMessage) {
-        return errorMessage
-    }
-
-    updateUsers(users) {
-        return users;
-    }
-
-    addUser(user) {
-        return user;
-    }
-    updateAddUser(message) {
-        return message;
-    }
-    errorAddUser(errorMessage) {
-        return errorMessage
-    }
-
-    removeUser(user) {
-        return user;
-    }
-    updateRemoveUser(message) {
-        return message;
-    }
-    errorRemoveUser(errorMessage) {
-        return errorMessage
-    }
-
-    deleteSlice(slice) {
-        return slice;
-    }
-    updateDeleteSlice(message) {
-        return message;
-    }
-    errorDeleteSlice(errorMessage) {
-        return errorMessage
-    }
-
-    errorUsers(errorMessage) {
-        return errorMessage
-    }
-
-    updateSlices(slices) {
-        return slices;
-    }
-
-    errorSlices(errorMessage) {
-        return errorMessage
-    }
-
-    showDialog(dialog) {
-        return dialog;
-    }
-
-    deleteProject(project) {
-        return project
-    }
-    updateDeleteProject(project) {
-        return project
-    }
-    errorDeleteProject(project) {
-        return project
-    }
 }
 
 export default alt.createActions(ProjectsActions);
