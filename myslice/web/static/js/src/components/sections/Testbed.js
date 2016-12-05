8a445decba673222324df6c0a4b294690f1c7154
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2016-12-05 18:13:40 +0100

    improved testbeds panel design

--- myslice/web/static/js/src/components/sections/Testbed.js
+++ myslice/web/static/js/src/components/sections/Testbed.js
@@ -7,31 +7,54 @@ import { Section, SectionHeader, SectionBody, SectionTitle, SectionOptions } fro
 
 const TestbedSectionPanel = ({testbeds, listOptions}) => {
     let technologies = [];
+    let labels = [];
     // get the list of technologies defined for the testbeds
     testbeds.map(function(testbed) {
         if ((testbed.type == 'AM') && (testbed.technologies !== "undefined")) {
             technologies = technologies.concat(testbed.technologies);
         }
     });
-    // there might be duplicates
-    technologies = technologies.filter((item, index, arr) => arr.indexOf(item) === index );
+    // there might be duplicates (low case first)
+    technologies = technologies.map(t => t.toLowerCase()).filter((item, index, arr) => arr.indexOf(item) === index );
+
+    technologies.forEach(function(t) {
+        switch(t) {
+            case 'iot':
+                labels[t] = 'IoT';
+                break;
+            case 'vm':
+                labels[t] = 'Virtual Machines';
+                break;
+            default:
+                labels[t] = t.charAt(0).toUpperCase() + t.slice(1);
+                break;
+        }
+    });
 
     return <Section>
         <SectionBody>
             <div className="row">
             {
                 technologies.map((technology) => {
-                    return <div key={"testbed-section-panel-" + technology} className="col-sm-6 technologyBox">
-                        <TechnologyIcon name={technology} />
-                        {
-                            testbeds.filter(function (testbed) {
-                                if ((testbed.type == 'AM') && (testbed.technologies !== "undefined")) {
-                                    return testbed.technologies.includes(technology);
-                                }
-                            }).map(function (testbed) {
-                                return <TestbedLabel key={"testbed-label-" + testbed.id} testbed={testbed} options={listOptions} />;
-                            })
-                        }
+                    return <div key={"testbed-section-panel-" + technology} className="col-sm-10 col-sm-offset-1 technologyBox">
+                        <div className="row">
+                            <div className="col-sm-4 technologyLabel">
+                                <img className={"technologyIcon " + technology} src={"/static/icons/technologies/" + technology + ".svg"} />
+                                <br />
+                                {labels[technology]}
+                            </div>
+                            <div className="col-sm-8 technologyTestbeds">
+                            {
+                                testbeds.filter(function (testbed) {
+                                    if ((testbed.type == 'AM') && (testbed.technologies !== "undefined")) {
+                                        return testbed.technologies.includes(technology);
+                                    }
+                                }).map(function (testbed) {
+                                    return <TestbedLabel key={"testbed-label-" + testbed.id} testbed={testbed} options={listOptions} />;
+                                })
+                            }
+                            </div>
+                        </div>
                     </div>
                 })
 
