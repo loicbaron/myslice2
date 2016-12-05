af0152f396ba338d444102e55b57af7344c3ae55
amirabradai <amirabradai@gmail.com>
2016-12-05 22:57:00 +0100

    fix warning

--- myslice/web/static/js/src/actions/AddOrganizationActions.js
+++ myslice/web/static/js/src/actions/AddOrganizationActions.js
@@ -5,6 +5,45 @@ import alt from '../alt';
 
 class AddOrganizationActions {
 
+    updateEmail(email) {
+        return email;
+    }
+
+    updatePassword(password) {
+        console.log("action pass");
+        return password;
+    }
+
+    updateFirstname(first_name) {
+        return first_name;
+    }
+
+    updateLastname(last_name) {
+        return last_name;
+    }
+
+    updateTerms(terms) {
+        return terms;
+    }
+
+    loading(loading) {
+        return loading;
+    }
+
+    submitForm() {
+        this.loading(true);
+        return true;
+    }
+
+    submitSuccess(response) {
+        this.loading(false);
+        return response;
+    }
+
+    submitError(response) {
+        this.loading(false);
+        return response;
+    }
 
 }
 
--- myslice/web/static/js/src/stores/AddOrganizationStore.js
+++ myslice/web/static/js/src/stores/AddOrganizationStore.js
@@ -7,7 +7,73 @@ import actions from '../actions/AddOrganizationActions';
 
 class AddOrganizationStore {
 
+constructor() {
+        this.authority = '';
+        this.email = '';
+        this.password = '';
+        this.first_name = '';
+        this.last_name = '';
+        this.terms = false;
+        this.loading = false;
+        this.success = false;
+        this.errorMessage = null;
+        this.bindListeners({
 
+            updateEmail: actions.UPDATE_EMAIL,
+            updatePassword: actions.UPDATE_PASSWORD,
+            updateFirstname: actions.UPDATE_FIRSTNAME,
+            updateLastname: actions.UPDATE_LASTNAME,
+            updateTerms: actions.UPDATE_TERMS,
+            updateLoading: actions.LOADING,
+            submitForm: actions.SUBMIT_FORM,
+            submitSuccess: actions.SUBMIT_SUCCESS,
+            submitError: actions.SUBMIT_ERROR,
+        });
+
+      //  this.registerAsync(source);
+    }
+
+
+
+    updateEmail(email) {
+        console.log("update email = "+email);
+        this.email = email;
+    }
+    updatePassword(password) {
+        console.log("update password");
+        this.password = password;
+    }
+    updateFirstname(first_name) {
+        console.log("update firstname = "+first_name);
+        this.first_name = first_name;
+    }
+
+    updateLastname(last_name) {
+        console.log("update lastname = "+last_name);
+        this.last_name = last_name;
+    }
+
+    updateTerms(terms) {
+        this.terms = terms;
+    }
+
+    updateLoading(loading) {
+        this.loading = loading;
+    }
+
+    submitForm() {
+        if (!this.getInstance().isLoading()) {
+            this.getInstance().submit();
+        }
+    }
+
+    submitSuccess(response) {
+        this.success = true;
+    }
+
+    submitError(response) {
+        this.errorMessage = response.data.error;
+    }
 
 
 
