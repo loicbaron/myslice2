40d088197453340ecca95ece1f1d769bfc973061
Amira <amira.bradai@lip6.fr>
2016-12-19 09:58:56 +0100

    label Organization and user (CSS to be fixed later)

--- myslice/web/static/js/src/components/AddOrganization.js
+++ myslice/web/static/js/src/components/AddOrganization.js
@@ -1,9 +1,8 @@
 /**
  * Created by amirabradai on 29/11/2016.
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
-//var React = require('react');
+
+var React = require('react');
 import InputText from './InputText';
 import actions from '../actions/AddOrganizationActions';
 import store from '../stores/AddOrganizationStore';
@@ -25,7 +24,7 @@ class AddOrganization extends React.Component {
     }
 
     onChange(state) {
-        console.log("changed");
+       // console.log("changed");
         this.setState(state);
     }
     updateEmail(value) {
@@ -49,17 +48,20 @@ class AddOrganization extends React.Component {
          //extract domains
          var nu=this.state.email.split('@');
          this.state.domains.push(nu[1]);
-         console.log(this.state.domanis)
+         console.log(this.state.domains)
         //actions.submitForm();
     }
     render() {
-         var emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        // var emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         return (
                     <form onSubmit={this.submitForm}>
 
                         <div className="row">
-                            <div className="col-sm-7 col-sm-offset-3">
+                            <div className="col-md-3">
+                                <i className="fa fa-university" aria-hidden="true"></i>
+                            </div>
+                            <div className="col-sm-9">
                                 <p className="inputDescription">
                                    Please provide information about your organization and you will be as a manager.
                                    OneLab is open to enterprise, to scientific researchers, and to educators.
@@ -74,7 +76,10 @@ class AddOrganization extends React.Component {
                             <InputText name="web" placeholder="http://" />
                         </div>
                     <div className="row">
-                        <div className="col-sm-7 col-sm-offset-3">
+                        <div className="col-md-3">
+                            <i className="fa fa-user" aria-hidden="true"></i>
+                        </div>
+                        <div className="col-sm-9">
                             <p className="inputDescription">
                                 Please provide your First name, your last name and Email address (your identifier for logging in). <br />
                                 We will also contact you to verify your account and occasionally for important communications.
@@ -93,7 +98,7 @@ class AddOrganization extends React.Component {
                     </div>
 
                     <div className="row">
-                        <InputText name="email" handleChange={this.updateEmail} placeholder="Email address" regex={emailRegExp} message="Invalid email" />
+                        <InputText name="email" handleChange={this.updateEmail} placeholder="Email address"  message="Invalid email" />
                     </div>
                     <div className="row">
                         <InputText name="password" handleChange={this.updatePassword} placeholder="Password" regex=".{8,}$" message="must be at least 8 characters" type="password" />
