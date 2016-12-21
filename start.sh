02d5149d4c3e91db8e0158d37cfb95f01e159fbd
Loic Baron <loic.baron@lip6.fr>
2016-12-21 19:47:23 +0100

    start server first

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@
 # Path of the script
 DIR=$(dirname "$0")
 
+# As deamon 
+$DIR/myslice/bin/myslice-server &
 #As deamon 
 $DIR/myslice/bin/myslice-live &
 #As deamon 
 $DIR/myslice/bin/myslice-monitor &
-# As deamon 
-$DIR/myslice/bin/myslice-server &
 #As deamon
 $DIR/myslice/bin/myslice-web &
