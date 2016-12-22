ddafba995f1802023b9f38b94e7a2486ebfcfda9
Loic Baron <loic.baron@lip6.fr>
2016-12-22 15:38:23 +0100

    tests echo trying to get localhost ports

--- tests/echo.py
+++ tests/echo.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3.5
-print("OK test should work")
+import time
+time.sleep(10)
+
+print("test should work")
+import requests
+r = requests.get('http://localhost:8111')
+print(r.text)
+r = requests.get('http://localhost:8080')
+print(r.text)
