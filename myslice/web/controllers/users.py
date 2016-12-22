5b9f80beffffae43f7731904e1d99d87321a8331
Loic Baron <loic.baron@lip6.fr>
2016-12-22 11:08:08 +0100

    controller user: get id optional

--- myslice/web/controllers/users.py
+++ myslice/web/controllers/users.py
@@ -2,5 +2,5 @@ from tornado import web
 from myslice.web.controllers import BaseController
 
 class Users(BaseController):
-    def get(self, id):
+    def get(self, id=None):
         self.render(self.application.templates + "/users.html")
