95b7c146d854fe0f402fd2c0fb3ccce12f8b1d4d
Amira <amira.bradai@lip6.fr>
2017-01-09 12:20:18 +0100

    config

--- myslice/services/finterop/config.py
+++ myslice/services/finterop/config.py
@@ -1,2 +1,10 @@
-RABBIT_HOST= 'localhost'
-QUEUE_TOPIC = 'URR'
+
+
+
+RABBIT_HOST='f-interop.rennes.inria.fr'
+
+# 'rabbit.dev.f-interop.eu:5672'
+QUEUE_TOPIC = 'control.resource_repository.service'
+VHOST='/'
+USERNAME='f-interop'
+PASSWORD='Cowabunga'
