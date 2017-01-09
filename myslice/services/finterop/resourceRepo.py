e92952bbc5ff4ea9c8bfa1a48356a8d88e45256e
Amira <amira.bradai@lip6.fr>
2017-01-09 12:03:35 +0100

    Sending RR insert request

--- myslice/services/finterop/resourceRepo.py
+++ myslice/services/finterop/resourceRepo.py
@@ -5,44 +5,26 @@ import json
 import config as cfg
 
 # Connect to RabbitMQ and create channel
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=cfg.RABBIT_HOST))
+
+credentials = pika.PlainCredentials('f-interop', 'Cowabunga')
+parameters = pika.ConnectionParameters('f-interop.rennes.inria.fr',
+                                       5672,
+                                       '/',
+                                       credentials)
+connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
 # Declare queue to send data
 channel.queue_declare(queue=cfg.QUEUE_TOPIC)
 
 data = {
-"routing_key": "resource.repository",
-"src_id": "amqp://gui@finterop.org",
-"msg_id": "9b70cd6b-4a2e-45f7-bb28-12cfd9788f16",
-"token": "9b70cd6b-4a2e-45f7-bb28-12cfd9788f16",
-"timestamp": "1469806702",
-"payload":
-    {
-    "_type": "rr_insert",
-    "owner_id": "Mandat",
-    "privacy_flag": "false",
-    "manufacturer": "Advanticsys",
-    "model": "CM5000",
-    "hw_platform": "TelosB",
-    "mac_id": "00-00-00-00-12-34",
-    "power_supply": "1",
-    "iut_version": "1.0.0",
-    "reference_status": "1",
-    "availability_status": "1",
-    "ipv6_address": "2001:620:607:5800::2e",
-    "media": "IEEE 802.15.4",
-    "protocol_name": "CoAP",
-    "protocol_version": "18",
-    "latitude": "46.2",
-    "longitude": "6.15",
-    "x": "1",
-    "y": "2",
-    "z": "3",
-    "characteristics_value": "char_value_1",
-    "characteristics_unit": "char_unit_1",
-    "parameter_name": "param"
-    }
+"_type": "resource_repository.insert_resource",
+"resource": {
+        "resource_id": "resource-001-unique-id",
+        "owner_id": "user-001-unique-id",
+        "privacy_flag": "public",
+
+}
 }
 
 message = json.dumps(data)
