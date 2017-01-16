9d9d185ba01df5ddf5f74aa5c9de242bf5cf969e
Loic Baron <loic.baron@lip6.fr>
2017-01-16 06:06:06 +0100

    start web after server

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ DIR=$(dirname "$0")
 
 # As deamon 
 $DIR/myslice/bin/myslice-server &
+#As deamon
+$DIR/myslice/bin/myslice-web &
 #As deamon 
 $DIR/myslice/bin/myslice-live &
 #As deamon 
 $DIR/myslice/bin/myslice-monitor &
-#As deamon
-$DIR/myslice/bin/myslice-web &
 
 ps -aux | grep 'myslice'
 tail -100 /var/log/myslice/myslice-web.log
