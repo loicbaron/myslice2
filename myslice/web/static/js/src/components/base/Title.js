7744d0d58db134b8d0a3249302960f9f602d8c0e
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2017-01-23 12:00:39 +0100

    Refactored View base component

--- myslice/web/static/js/src/components/base/Title.js
+++ myslice/web/static/js/src/components/base/Title.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Title = ({title, subtitle}) =>
-    <h2>{title} <span>{subtitle}</span></h2>;
+const Title = ({title, subtitle, separator}) => {
+    let sub = null;
+    let sep = null;
+
+    if (subtitle) {
+        sub = <span className="subtitle">{subtitle}</span>;
+    }
+
+    if (separator) {
+        sep = <span className="separator">{separator}</span>;
+    }
+    return <h2>{title}{sep}{sub}</h2>;
+
+};
 
 Title.propTypes = {
     title: React.PropTypes.string.isRequired,
--- myslice/web/static/js/src/components/base/View.js
+++ myslice/web/static/js/src/components/base/View.js
@@ -4,76 +4,135 @@ import { NotifyInfo, NotifySuccess, NotifyError, NotifyWarning  } from './Notifi
 
 const View = ({children, notification}) => {
 
-    let num = React.Children.count(children);
     let notificationElement = null;
 
     if (notification) {
-            switch(notification.type) {
-                case 'info':
-                    notificationElement = <NotifyInfo message={notification.message} />;
-                    break;
-                case 'success':
-                    notificationElement = <NotifySuccess message={notification.message} />;
-                    break;
-                case 'error':
-                    notificationElement = <NotifyError message={notification.message} />;
-                    break;
-                case 'warning':
-                    notificationElement = <NotifyWarning message={notification.message} />;
-                    break;
-            }
+        switch(notification.type) {
+            case 'info':
+                notificationElement = <NotifyInfo message={notification.message} />;
+                break;
+            case 'success':
+                notificationElement = <NotifySuccess message={notification.message} />;
+                break;
+            case 'error':
+                notificationElement = <NotifyError message={notification.message} />;
+                break;
+            case 'warning':
+                notificationElement = <NotifyWarning message={notification.message} />;
+                break;
+        }
 
     }
 
+    return (
+        <div className="view">
+            {children}
+            {notificationElement}
+        </div>
+    );
+
+};
+
+const ViewHeader = ({children}) => {
+
+    let num = React.Children.count(children);
+
+    if (num == 1) {
+        return (
+            <div className="view-header">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+                            {children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    } else if (num == 2) {
+        return (
+            <div className="view-header">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm-8">
+                            {children[0]}
+                        </div>
+                        <div className="col-sm-4">
+                            {children[1]}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+};
+
+const ViewBody = ({children}) => {
+
+    let num = React.Children.count(children);
 
     if (num == 1) {
 
-        return <div className="p-center">
+        return <div className="view-body p-center">
                     {children}
-                    {notificationElement}
                 </div>;
 
     } else if (num == 2) {
         let leftPanel = children[0].type.name;
 
         if (leftPanel == 'Panel') {
-            return <div className="row">
-                    <div className="col-sm-6">
-                        <div className="p-left">
-                            {children[0]}
-                        </div>
+
+            return (
+                <div className="view-body">
+                    <div className="view-left">
+                        {children[0]}
                     </div>
-                    <div className="col-sm-6">
-                         <div className="p-right">
-                             {children[1]}
-                         </div>
+                    <div className="view-right">
+                        {children[1]}
                     </div>
-                    {notificationElement}
-                </div>;
+                </div>
+            );
+
         } else if (leftPanel == 'PanelMenu') {
-            return <div className="row">
-                    <div className="col-sm-3">
-                        <div className="p-menu">
-                            {children[0]}
+
+            return (
+                <div className="view-body">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-sm-3">
+                                <div className="p-menu">
+                                    {children[0]}
+                                </div>
+                            </div>
+                            <div className="col-sm-6">
+                                <div className="p-center">
+                                    {children[1]}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-sm-6">
-                        <div className="p-center">
-                            {children[1]}
-                        </div>
-                    </div>
-                {notificationElement}
-            </div>
+                </div>
+            );
 
         }
     } else {
         return (
-                <div className="p-center">
-                    not supported (too many children)
-                </div>
+            <div className="view-body p-center">
+                not supported (too many children)
+            </div>
         );
     }
     
 };
 
-export default View;
+const Panel = ({children}) =>
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-12">
+                {children}
+            </div>
+        </div>
+    </div>;
+
+export { View, ViewHeader, ViewBody, Panel };
