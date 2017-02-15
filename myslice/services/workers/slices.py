efe6f741d9e568c7905dee76bf4b14bf4eb14ff3
Loic Baron <loic.baron@lip6.fr>
2017-02-15 11:07:39 +0100

    workers slices: logs to follow updates using user's Credentials (for the resources)

--- myslice/services/workers/slices.py
+++ myslice/services/workers/slices.py
@@ -186,15 +186,19 @@ def syncSlices(id=None):
             logger.warning("Query slices is empty, check myslicelib and the connection with SFA Registry")
 
         for slice in slices:
+            logger.info("Synchronize slice %s:" % slice.hrn)
             if len(slice.users) > 0:
                 try:
                     u = User(db.get(dbconnection, table='users', id=slice.users[0]))
+
+                    logger.info("with user %s:" % u.hrn)
 
                     # Synchronize resources of the slice only if we have the user's private key or its credentials
                     # XXX Should use delegated Credentials
                     #if (hasattr(u,'private_key') and u.private_key is not None and len(u.private_key)>0) or (hasattr(u,'credentials') and len(u.credentials)>0):
                     if u.private_key or (hasattr(u,'credentials') and len(u.credentials)>0):
                         user_setup = UserSetup(u,myslicelibsetup.endpoints)
+                        logger.info("Slice.id(%s).get() with user creds" % slice.hrn)
                         s = q(Slice, user_setup).id(slice.id).get().first()
                         db.slices(dbconnection, s.dict(), slice.id)
                 except Exception as e:
@@ -207,7 +211,6 @@ def syncSlices(id=None):
 
         # update local slice table
         if not id:
-            # update local slice table
             if len(slices)>0:
                 lslices = db.slices(dbconnection, slices.dict())
 
@@ -229,3 +232,5 @@ def syncSlices(id=None):
         #import traceback
         #traceback.print_exc()
         logger.exception(str(e))
+
+    dbconnection.close()
