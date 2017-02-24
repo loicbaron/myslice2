6c58dab666866750b5ee3d19a2be02cfb9c80700
Loic Baron <loic.baron@lip6.fr>
2017-02-24 13:44:19 +0100

    GitLab CI is it the good one?

--- init_user.py
+++ init_user.py
@@ -5,10 +5,15 @@ import sys, getopt
 import rethinkdb as r
 import threading
 from myslice import db
+from myslice.db.user import User
+from myslicelib.query import q
 from myslice.web.controllers.login import crypt_password
 from pprint import pprint
 from myslice.services.workers.users import syncUsers
 
+from tornado import gen, escape
+
+@gen.coroutine
 def main(argv):
     try:
        opts, args = getopt.getopt(argv,"he:P:k:p:s",["email=","password=","private_key=","public_key=","sync="])
@@ -40,22 +45,39 @@ def main(argv):
          if arg.lower() in ["true", "yes", "y"]:
             sync = True
    
-    # Synchronize the users from SFA Registry into the DB 
-    lock = threading.Lock()
-    print("sync user %s" % email)
-    if sync:
-        syncUsers(lock, email=email, job=False)
 
-    # Get the user that we want to update
     dbconnection = db.connect()
-    result = r.db('myslice').table('users').filter({'email':email}, default=False).run(dbconnection)
-    pprint(result)
-    for u in result: 
-        u['password']=password
-        u['private_key']=private_key
-        u['public_key']=public_key
-        u['generate_keys']=False
-        r.db('myslice').table('users').get(u['id']).update(u).run(dbconnection)
+
+    lock = threading.Lock()
+    # Synchronize the users from SFA Registry into the DB
+    if sync:
+        print("sync user %s" % email)
+        syncUsers(lock, email=email, job=False)
+    else:
+        try:
+            # Get the user from SFA Registry
+            print("get user %s from SFA Reg" % email)
+            remote_user = q(User).filter('email', email).get().first()
+            print(remote_user.hrn)
+            # merge fields from script with remote_user
+            remote_user.setAttribute('password', password)
+            remote_user.setAttribute('private_key', private_key)
+            remote_user.setAttribute('generate_keys', False)
+            remote_user.setAttribute('public_key', public_key)
+            remote_user.setAttribute('keys', [public_key])
+            result = remote_user.save(dbconnection)
+            if result:
+                print("User saved")
+            else:
+                print("Error during save")
+                pprint(result)
+            # if user has private key
+            # update its Credentials
+            #if 'private_key' in updated_user:
+            #    updated_user = update_credentials(updated_user)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
