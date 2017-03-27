e7cea4cfc153c1ea8f9309a0365fba83f669e97e
Loic Baron <loic.baron@lip6.fr>
2017-03-27 17:01:49 +0200

    myslice init needs /etc/myslice/main.cfg to be configured

--- myslice/__init__.py
+++ myslice/__init__.py
@@ -23,7 +23,7 @@ for k,endpoint in config.endpoints.items():
             Endpoint(
                 url = endpoint["url"],
                 type = endpoint["type"],
-                timeout = endpoint["timeout"],
+                timeout = int(endpoint["timeout"]),
                 name = endpoint["name"],
                 technologies = endpoint["technologies"]
             )
@@ -41,44 +41,16 @@ myslicelibsetup.registry_endpoints = [
 #    cert = path + "onelab.upmc.loic_baron.user.gid"
 #else:
 #    path = os.path.expanduser("~/")
-path = "/var/myslice/"
-pkey = path + "myslice.pkey"
-hrn = "onelab.myslice"
-email = "support@myslice.info"
-cert = path + "myslice.cert"
-
+pkey = config.auth['pkey']
+cert = config.auth['cert']
+hrn = config.auth['hrn']
+email = config.auth['email']
 
 myslicelibsetup.authentication = Authentication(hrn=hrn, email=email, certificate=cert, private_key=pkey)
-
-class settings(object):
-
-    ## setup db
-    # @property
-    # def db(self):
-    #     return dbsettings()
-
-    class db(object):
-        host = "localhost"
-        port = 28015
-        name = "myslice"
-
-    class email(object):
-        dirpath = '/myslice/myslice/web/templates/email'
-        theme = 'onelab'
-        domain = 'onelab.eu'
-        host = 'smtp.gmail.com'
-        port = 587
-        ssl = True
-        user = 'zhouquantest16@gmail.com'
-        password = 'zqtest123'
-
-    class web(object):
-        url = 'http://dev.myslice.info'
-        port = '8111'
-        cookie_secret="x&7G1d2!5MhG9SWkXu"
-        token_secret = 'u636vbJV6Ph[EJB;Q'
 
 ## override with configuration file
 #parser = SafeConfigParser()
 #s = parser.read(self.files)
 #parser.get(section, key)
+
+settings = config
