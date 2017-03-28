0f32329f071f50ae495672638ef2d69459672df5
Loic Baron <loic.baron@lip6.fr>
2017-03-28 12:55:55 +0200

    tests config for gitlab-ci using myslice default config

---
+++ myslice/tests/config.myslice.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.5
+import json
+import requests
+
+s = {}
+server = 'localhost'
+s['email'] = "support@myslice.info"
+s['password'] = "my_password"
+s['hrn'] = 'onelab.myslice'
+payload = {'email': s['email'], 'password': s['password']}
+r = requests.post("http://"+server+":8111/api/v1/login", headers={str('Content-Type'):'application/json'}, data=json.dumps(payload))
+s['cookies'] = r.cookies
+s['automate_test'] = False
+
+#authority = "urn:publicid:IDN+onelab:upmc+authority+sa"
+authority = "urn:publicid:IDN+onelab+authority+sa"
+rootAuthority = "urn:publicid:IDN+onelab+authority+sa"
+
+#on this project we will test creating deleting slices
+project = "urn:publicid:IDN+onelab:upmc:testradomir+authority+sa"
+
+
