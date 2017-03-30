9593f5bd05ba26b316708ede9dbb8741c5c55b50
amirabradai <amirabradai@gmail.com>
2017-03-30 16:30:36 +0200

    new config for tester

--- myslice/tests/config.py
+++ myslice/tests/config.py
@@ -3,19 +3,20 @@ import json
 import requests
 
 s = {}
-server = 'zeus.noc.onelab.eu'
-s['email'] = "loic.baron@lip6.fr"
-s['password'] = ""
-s['hrn'] = 'onelab.upmc.loic_baron'
+server = 'theseus.onelab.noc.eu'
+s['email'] = "support@myslice.info"
+s['password'] = "my_password"
+s['hrn'] = 'onelab.myslice'
+print("config trying to login to %s" % server)
 payload = {'email': s['email'], 'password': s['password']}
 r = requests.post("http://"+server+":8111/api/v1/login", headers={str('Content-Type'):'application/json'}, data=json.dumps(payload))
 s['cookies'] = r.cookies
-s['automate_test'] = True
+s['automate_test'] = False
 
-authority = "urn:publicid:IDN+onelab:upmc+authority+sa"
+#authority = "urn:publicid:IDN+onelab:upmc+authority+sa"
+authority = "urn:publicid:IDN+onelab+authority+sa"
 rootAuthority = "urn:publicid:IDN+onelab+authority+sa"
 
-#on this project we will test creating deletin slices
+#on this project we will test creating deleting slices
 project = "urn:publicid:IDN+onelab:upmc:testradomir+authority+sa"
-
 
