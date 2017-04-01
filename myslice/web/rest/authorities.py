ac778e140bd10419e06daaa901a7cd5959ec9cf2
Loic Baron <loic.baron@lip6.fr>
2017-04-01 15:50:44 +0200

    REST authorities using isAdmin function to check rights

--- myslice/web/rest/authorities.py
+++ myslice/web/rest/authorities.py
@@ -129,8 +129,7 @@ class AuthoritiesHandler(Api):
                 if not a:
                     self.userError("this authority %s does not exist" % id)
                     return
-                root_auth = yield r.table('authorities').get(a['authority']).run(self.dbconnection)
-                if self.current_user['id'] not in a['pi_users'] and self.current_user['id'] not in root_auth['pi_users']:
+                if self.current_user['id'] not in a['pi_users'] and not self.isAdmin():
                     self.userError("your user has no rights on authority: %s" % id)
                     return
             except Exception:
@@ -306,19 +305,18 @@ class AuthoritiesHandler(Api):
         :return:
         """
         try:
-            # Check if the user has the right to Update an authority, PI of an upper authority
+            # Check if the user has the right to Update an authority
             a = yield r.table('authorities').get(id).run(self.dbconnection)
             if not a:
                 self.userError("this authority %s does not exist" % id)
                 return
-            root_auth = yield r.table('authorities').get(a['authority']).run(self.dbconnection)
-            # TBD: admin or pi?
-            #if self.current_user['id'] not in a['pi_users'] and self.current_user['id'] not in root_auth['pi_users']:
-            # only root_auth admin at the moment
-            if self.current_user['id'] not in root_auth['pi_users']:
+            # only admin or pi?
+            if self.current_user['id'] not in a['pi_users'] and not self.isAdmin(): 
                 self.userError("your user has no rights on authority: %s" % id)
                 return
         except Exception:
+            import traceback
+            traceback.print_exc()
             self.userError("not authenticated ")
             return
 
