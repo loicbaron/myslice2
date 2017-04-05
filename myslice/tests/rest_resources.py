291c1b0ab765d9c03f14e799351d35d8e7a928c3
Loic Baron <loic.baron@lip6.fr>
2017-04-05 11:52:40 +0200

    Tests GET Resources

---
+++ myslice/tests/rest_resources.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3.5
+
+import json
+import requests
+import time
+import unittest
+
+from pprint import pprint
+from random import randint
+
+from myslice.tests import LocalTestCase
+from myslice.tests.config import s, authority, server, project
+from datetime import datetime
+
+class TestResources(LocalTestCase):
+
+    testbed = None
+
+    def setUp(self):
+        self.automateTest = s['automate_test']
+        self.startTimer()
+        self.timeout = 10
+        self.cookies = s['cookies']
+
+    def tearDown(self):
+        self.stopTimer()
+
+    def test_0_getNoAuth(self):
+        r = requests.get('http://'+server+':8111/api/v1/resources')
+        self.assertEqual(r.status_code, 400)
+
+    def test_1_getAllResources(self):
+        r = requests.get('http://'+server+':8111/api/v1/resources', cookies=self.cookies)
+        self.assertEqual(r.status_code, 200)
+
+    def test_1_getTestbeds(self):
+        r = requests.get('http://'+server+':8111/api/v1/testbeds', cookies=self.cookies)
+        self.assertEqual(r.status_code, 200)
+        data = json.loads(r.text)
+        self.__class__.testbed = data['result'][0]
+
+    def test_2_getTestbedResources(self):
+        testbed = self.__class__.testbed
+        r = requests.get('http://'+server+':8111/api/v1/testbeds/'+testbed+'/resources', cookies=self.cookies)
+        self.assertEqual(r.status_code, 200)
+
