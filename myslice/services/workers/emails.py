488f69d7f126f9084b20507477ec8a27e3f0453b
Radomir Klacza <radomir.klacza@lip6.fr>
2017-07-11 17:14:28 +0200

    Activity and router

--- myslice/services/workers/emails.py
+++ myslice/services/workers/emails.py
@@ -211,7 +211,7 @@ def sendEmail(event, recipients, subject, template, url, buttonLabel):
     # use premailer module to get CSS inline
     mail_body_inline = transform(mail_body.decode())
     logger.debug("about mail Message")
-    m = Message(mail_from=[s.email['name']+' Support', s.email['sender']],
+    m = Message(mail_from=[s.email['sender']],
                 mail_to = mail_to,
                 subject = subject,
                 html_content = mail_body_inline
