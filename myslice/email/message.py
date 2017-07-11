bd06030b29eb66c9940419e18129356fc0e94b61
Radomir Klacza <radomir.klacza@lip6.fr>
2017-07-11 16:31:22 +0200

    mail fixing

--- myslice/email/message.py
+++ myslice/email/message.py
@@ -143,10 +143,10 @@ class Mailer(object):
         if s.email['password'] is not None:
             logger.debug("logging with username and password {} ".format(s.email['password']))
             self.server.login(s.email['user'], s.email['password'])
-        logger.debug("Sending email - send method 2{}".format(message.as_string))
+        logger.debug("Sending email - send method 2{} {}".format(message.mail_to(), message.as_string()))
 
-        self.server.sendmail(s.email['sender'], message.mail_to , "Test")
-        logger.debug("Sending email - send method 3{}".format(message.as_string))
+        self.server.sendmail(s.email['sender'], message.mail_to(), message.as_string())
+        logger.debug("Sending email - send method 3{}".format(message.as_string()))
 
         self.server.quit()
         
