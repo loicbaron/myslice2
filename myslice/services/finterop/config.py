6af45aee9a0e9fb0b1db74b699cfe9a8169f2085
Radomir Klacza <radomir.klacza@lip6.fr>
2017-07-24 14:24:40 +0200

    removing finterop from master branch

--- myslice/services/finterop/config.py
+++
@@ -1,9 +0,0 @@
-
-
-
-RABBIT_HOST='f-interop.rennes.inria.fr'
-# 'rabbit.dev.f-interop.eu:5672'
-QUEUE_TOPIC = 'control.resource_repository.service'
-VHOST='/'
-USERNAME='f-interop'
-PASSWORD='Cowabunga'
